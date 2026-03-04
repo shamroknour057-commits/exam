b64a43a32c439fa7b82c24b1a7dc6101e548a031
nourshaamrok@gmail.com <shamroknour057@gmail.com>
2026-03-04 20:54:21 +0200

    fix

--- controllers/feesHelper.js
+++ controllers/feesHelper.js
@@ -1,23 +1,36 @@
-const { feesData } = require('../models/feesStore');
+function getSortedCustomers(feesData = {}) {
+  const customersSet = new Set();
 
-function getSortedCustomers() {
-  return Array.from(new Set(
-    Object.values(feesData).flatMap(obj => Object.keys(obj))
-  )).sort();
+  for (const dateData of Object.values(feesData)) {
+    if (!dateData || typeof dateData !== 'object') continue;
+
+    for (const customer of Object.keys(dateData)) {
+      customersSet.add(customer);
+    }
+  }
+
+  return Array.from(customersSet).sort();
 }
 
-function getSortedDates() {
+function getSortedDates(feesData = {}) {
   return Object.keys(feesData).sort();
 }
 
-function buildPivotRows(customers, dates) {
-  return dates.map(date => {
+function buildPivotRows(customers = [], dates = [], feesData = {}) {
+  const rows = [];
+
+  for (const date of dates) {
     const row = { date };
-    customers.forEach(cust => {
-      row[cust] = feesData[date][cust] !== undefined ? feesData[date][cust] : 0.00;
-    });
-    return row;
-  });
+    const dateData = feesData[date] ?? {};
+
+    for (const customer of customers) {
+      row[customer] = dateData[customer] ?? 0.0;
+    }
+
+    rows.push(row);
+  }
+
+  return rows;
 }
 
 module.exports = {
